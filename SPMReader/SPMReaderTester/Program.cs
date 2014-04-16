7dd9387558519840686c85678e3900726e601de3
mikejr83 <mikejr83@gmail.com>
2014-04-16 09:51:21 -0400

    Setup of the SPMReaderTester application. This application loads all the SPM files linked into the project and runs them against the SPMReader application.

---
+++ SPMReader/SPMReaderTester/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace SPMReaderTester
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      FileInfo assemblyFI = new FileInfo(typeof(MainClass).Assembly.Location);
+
+      var spmFileslist = Directory.EnumerateFiles(Path.Combine(assemblyFI.DirectoryName, "SPMFiles"), "*.SPM", SearchOption.AllDirectories).ToList();
+
+      foreach(string spmFilename in spmFileslist)
+      {
+        Console.WriteLine(string.Format("********** {0} **********", spmFilename));
+        ProcessStartInfo psi = new ProcessStartInfo("SPMReader.exe", string.Format("-Debug \"{0}\"", spmFilename));
+        psi.UseShellExecute = false;
+        psi.RedirectStandardError = true;
+        psi.RedirectStandardOutput = true;
+        psi.WindowStyle = ProcessWindowStyle.Minimized;
+
+        Process process = new Process();
+        process.StartInfo = psi;
+        process.ErrorDataReceived += process_ErrorDataReceived;
+        process.OutputDataReceived += process_OutputDataReceived;
+
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        process.WaitForExit();
+
+        Console.WriteLine();
+      }
+    }
+
+    static void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+    {
+      Console.Error.WriteLine(e.Data);
+    }
+
+    static void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+    {
+      Console.WriteLine(e.Data);
+    }
+  }
+}
